6e03fb6759827a6123b525ae53f1c6cb1a91fa72
xzj <xzj007@gmail.com>
2017-02-27 07:25:34 +0800

    Changed: Added csk.core/write-to and csk.core/read-from
    	modified:   src/csk/core.clj

--- src/csk/core.clj
+++ src/csk/core.clj
@@ -1,7 +1,9 @@
 (ns csk.core
-    (:require [csk.serializer :as csr])
+    (:require [csk.serializer :as csr]
+              [clojure.java.io :as jio])
     (:import
       (com.esotericsoftware.kryo Kryo Serializer)
+      (com.esotericsoftware.kryo.io Output Input)
       ))
 
 (defn register-serializers
@@ -16,3 +18,23 @@
    (kryo (Kryo.)))
   ([^Kryo k]
    (register-serializers k csr/csk-default-serializers)))
+
+(defn write-to
+  ([output-stream-able data]
+   (write-to (kryo) output-stream-able data))
+  ([^Kryo k output-stream-able data]
+   (let [os (jio/output-stream output-stream-able)
+         op (Output. os)
+         ]
+     (with-open [o op]
+       (.writeClassAndObject k o data)))))
+
+(defn read-from
+  ([input-stream-able]
+   (read-from (kryo) input-stream-able))
+  ([^Kryo k input-stream-able]
+   (let [is (jio/input-stream input-stream-able)
+         ip (Input. is)
+         ]
+     (with-open [i ip]
+       (.readClassAndObject k i)))))
